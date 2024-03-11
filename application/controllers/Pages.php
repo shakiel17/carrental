f82bd9b9bbf18bf007a4097aa03da95a2f510dab
shakiel17 <hisoka.aboy@gmail.com>
2024-03-11 08:56:35 +0800

    Carrental

---
+++ application/controllers/Pages.php
@@ -0,0 +1,27 @@
+<?php
+    ini_set('max_execution_time', 0);
+    ini_set('memory_limit','2048M');
+    date_default_timezone_set('Asia/Manila');
+    class Pages extends CI_Controller{
+        public function index(){
+            $page = "index";
+            if(!file_exists(APPPATH.'views/pages/'.$page.".php")){
+                show_404();
+            }
+            if($this->session->user_login){
+                redirect(base_url()."main");
+            }
+            $this->load->view('pages/'.$page);            
+        }        
+        public function admin(){
+            $page = "index";
+            if(!file_exists(APPPATH.'views/pages/admin/'.$page.".php")){
+                show_404();
+            }
+            if($this->session->user_login){
+                redirect(base_url()."admin/main");
+            }
+            $this->load->view('pages/admin/'.$page);            
+        }
+    }
+?>
---
+++ database/carrental.sql
@@ -0,0 +1,119 @@
+/*
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 10.1.38-MariaDB : Database - carrental
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`carrental` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `carrental`;
+
+/*Table structure for table `agreement` */
+
+DROP TABLE IF EXISTS `agreement`;
+
+CREATE TABLE `agreement` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `datearray` date DEFAULT NULL,
+  `rented_by` varchar(100) DEFAULT NULL,
+  `contactno` varchar(100) DEFAULT NULL,
+  `address` text,
+  `car_id` int(11) DEFAULT NULL,
+  `odometer` varchar(100) DEFAULT NULL,
+  `fuel_type` varchar(100) DEFAULT NULL,
+  `destination` varchar(100) DEFAULT NULL,
+  `rate` double DEFAULT NULL,
+  `payment_terms` varchar(100) DEFAULT NULL,
+  `washing` double DEFAULT NULL,
+  `totalamount` double DEFAULT NULL,
+  `date_rented` date DEFAULT NULL,
+  `time_rented` time DEFAULT NULL,
+  `date_return` date DEFAULT NULL,
+  `time_return` time DEFAULT NULL,
+  `signature` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `booking` */
+
+DROP TABLE IF EXISTS `booking`;
+
+CREATE TABLE `booking` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `customer_id` varchar(100) DEFAULT NULL,
+  `car_id` varchar(100) DEFAULT NULL,
+  `date_started` date DEFAULT NULL,
+  `time_started` time DEFAULT NULL,
+  `date_return` date DEFAULT NULL,
+  `time_return` time DEFAULT NULL,
+  `destination` varchar(100) DEFAULT NULL,
+  `status` varchar(100) DEFAULT 'pending',
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  `proof_of_payment` varchar(100) DEFAULT NULL,
+  `payment_type` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `cars` */
+
+DROP TABLE IF EXISTS `cars`;
+
+CREATE TABLE `cars` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `description` text,
+  `type` varchar(100) DEFAULT NULL,
+  `fuel_type` varchar(100) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `customer` */
+
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(100) DEFAULT NULL,
+  `firstname` varchar(100) DEFAULT NULL,
+  `middlename` varchar(100) DEFAULT NULL,
+  `address` text,
+  `contactno` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `facebook` varchar(100) DEFAULT NULL,
+  `valid_id_1` varchar(100) DEFAULT NULL,
+  `valid_id_2` varchar(100) DEFAULT NULL,
+  `proof_of_address` varchar(100) DEFAULT NULL,
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `fullname` varchar(100) DEFAULT NULL,
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
